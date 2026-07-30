28e950f2dd3c81449c0de70d53f42924685ac5ca
jyotishokeenn <jyotishokeen789@gmail.com>
2026-07-30 09:40:18 +0530

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,96 @@
+#==============LOAD MODULES==================
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
+import langchain
+from langchain.agents import create_agent
+import langchain_community
+from tavily import TavilyClient
+import pytesseract as pyt
+import streamlit as st
+import os
+import time
+from PIL import Image
+import pandas as pd
+import numpy as np
+#============API KEY==================
+model= ChatGoogleGenerativeAI(
+    model= 'gemini-3.5-flash-lite',
+    google_api_key= GOOGLE_API_KEY
+)
+
+response= model.invoke("Hello Buddy!")
+response.content[-1]['text']
+
+def search_latest_news_jobs(query):
+  """This function helps to fetch latest
+  news or jobs related article using
+  tavily"""
+
+  client= TavilyClient(
+      api_key= TAVILY_API_KEY)
+  response= client.search(query)
+  return response
+
+#=============Agent Creation=================
+agent= create_agent(
+     model= model,
+     tools= [search_latest_news_jobs])
+agent
+
+def main_agent(agent,query):
+  """This is main agent, or leader agent
+  orchestrate sub agents"""
+
+  #Giving prompts to create  detailed prompt for code generation
+  prompt= """You are AI assistant and below given a prompt, your task is to give
+  detailed prompt for this.
+  You are a professional Resume generator where uuser will give their personal
+  information, you have to create a detailed Resume for students or professional
+  one, it must be with Dynamic VI and VX and, with advanced CSS Professional
+  Desining. Make sure to give output in HTML Format only no markdowns allowed"""
+
+  response = agent.invoke({'messages': [{'role': 'user','content': prompt}]})
+  detailed_prompt = response['messages'][-1].content[-1]['text']
+  # SAVE PROMPT using File Handling
+  with open('prompt.txt', 'w') as f:
+     f.write(detailed_prompt)
+
+  user_details= f"""Below Given is a user details generate Resume based on that,
+  if not given keep:
+  Default Resume: Python Developer
+  user details:{query}"""
+  final_prompt= prompt+ detailed_prompt + user_details
+
+  #Code Generation
+
+  response = agent.invoke({'messages': [{'role': 'user','content': final_prompt}]})
+  code = response['messages'][-1].content[-1]['text']
+  return code
+
+code= main_agent(agent,"JYOTI SHOKEEN, GEN AI EXPERT")
+from IPython import display as DISPLAY
+DISPLAY.HTML(code)
+
+def get_job(agent,
+           Location= "Noida, Delhi",
+           Profile= "Data Analysts, AI Engineer"):
+  Location = "Noida, Delhi"
+  Profile = "Data Analysts, AI Engineer"
+
+  prompt= f"""Based user given Job profile,
+  fetch latest jobs or job apply article
+  using Naukri, Linkdin, Indeed, or all popular
+  Job apply platform , Show Results with
+  JOB PROFILE NAME, EDUCATION, SALARY, COMPANY NAME,
+  SHOW jobs only related to given
+  {Location} and {Profile}, output must be in
+  Professional HTML Naukri theme cards with Dynamic Design,
+  Show atleast Top 18-20 results with direct apply link"""
+
+  response = agent.invoke({'messages': [{'role': 'user','content': prompt}]})
+  code = response['messages'][-1].content[-1]['text']
+
+  return code
+
+code= get_job(agent)
+DISPLAY.HTML(code)
