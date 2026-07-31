17deb41435b85e35fdf675a119d0533442703f2d
jyotishokeenn <jyotishokeen789@gmail.com>
2026-07-31 09:19:31 +0530

    Refactor app.py to improve structure and clarity

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ user_info = st.text_area("""Write your Resume Description: """)
 if st.button("Generate Resume"):
           with st.spinner("Agent Running"):
                     code = main_agent(agent, user_info)
-                    st.html(code width="stretch",
+                    st.html(code, width="stretch",
                             unsafe_allow_javascript=True)
                     st.divider() # to give horizontal div
                     job_code = get_jobs (agent, location, profile)
